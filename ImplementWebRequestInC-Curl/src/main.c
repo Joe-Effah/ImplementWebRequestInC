f8cbf6b94774a5f6f8e41339eefe98b97f691acf
Obed Danso <obed.danso@scynett.com>
2025-04-04 07:18:44 +0000

    Initial Repo Setup

---
+++ ImplementWebRequestInC-Curl/src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello ImplementWebRequestInC-Curl");
+  return 0;
+} 
+
---
+++ ImplementWebRequestInC-Tcp-Ip/src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello ImplementWebRequestInC-Tcp-Ip");
+  return 0;
+} 
+
